f4af79824e97db4003e0484ede980dfe82bdc8d4
Vijay Hardaha <vijayhardaha@gmail.com>
2022-03-10 23:16:28 +0530

    Tweak: updated code logic
    - Removed chalk package
    - Removed chalk usage
    - Remove error throw to keep transform optional
    - Set new logics

--- index.js
+++ index.js
@@ -1,66 +1,94 @@
-const chalk = require('chalk');
 const { Transformer } = require('@parcel/plugin');
 
+/**
+ * Extract a meta from a given html string.
+ *
+ * @param {String} html File HTML.
+ * @param {String} property Property name.
+ * @returns {bool|String}
+ */
 const getMetaTag = (html, property) => {
   const regex = new RegExp(
     `<meta[^>]*property=["|']${property}["|'][^>]*>`,
     'i',
   );
   const results = regex.exec(html);
-
-  if (!results) {
-    throw new Error(`Missing ${property}`);
-  }
-
-  return results[0];
+  return results ? results[0] : false;
 };
 
+/**
+ * Extract the content of a given meta html.
+ *
+ * @param {String} metaTagHtml Meta tag html.
+ * @returns {bool|String}
+ */
 const getMetaTagContent = (metaTagHtml) => {
   const contentRegex = /content="([^"]*)"/i;
   const results = contentRegex.exec(metaTagHtml);
+  return results ? results[1] : false;
+};
 
-  if (!results) {
-    throw new Error(`Missing content attribute in ${chalk.bold(metaTagHtml)}`);
+/**
+ * Change the url of a meta by prepending the given baseUrl.
+ *
+ * @param {String} metaHTML Meta tag html.
+ * @param {String} baseUrl Base url(site url)
+ * @returns {String}
+ */
+const getReplacedMetaTagContent = (metaHTML, baseUrl) => {
+  const metaContent = getMetaTagContent(metaHTML);
+  // instead of url.resolve, we just append directly.
+  // the reason is it seems like Parcel 2 just goes through
+  // and replaces content hash with string replace and adds a `/`
+  // in front of it. So if we do url resolve then we get a double
+  // `//` and the link won't work.
+  const absoluteImageUrl = baseUrl + metaContent;
+  return metaHTML.replace(metaContent, absoluteImageUrl);
+};
+
+/**
+ * Find and replace meta.
+ *
+ * @param {String} contents Html content.
+ * @param {String} imageProperty Image property name(og:image, twitter:image)
+ * @param {String} urlProperty Url property name(og:url)
+ * @returns {String}
+ */
+const findReplaceMeta = (
+  contents,
+  imageProperty = 'og:image',
+  urlProperty = 'og:url',
+) => {
+  const urlTag = getMetaTag(contents, urlProperty);
+  if (!urlTag) {
+    return contents;
   }
 
-  return results[1];
+  const baseUrl = getMetaTagContent(urlTag);
+  if (!baseUrl) {
+    return contents;
+  }
+
+  // Fetch original meta.
+  const imageMeta = getMetaTag(contents, imageProperty);
+  if (imageMeta) {
+    return contents.replace(
+      imageMeta,
+      getReplacedMetaTagContent(imageMeta, baseUrl),
+    );
+  }
+
+  return contents;
 };
 
 module.exports = new Transformer({
   async transform({ asset }) {
     const html = await asset.getCode();
     try {
-      const ogImageTag = getMetaTag(html, 'og:image');
-      const ogImageContent = getMetaTagContent(ogImageTag);
-
-      const twitterImageTag = getMetaTag(html, 'twitter:image');
-      const twitterImageContent = getMetaTagContent(twitterImageTag);
-
-      const ogUrlTag = getMetaTag(html, 'og:url');
-      const ogUrlContent = getMetaTagContent(ogUrlTag);
-
-      // instead of url.resolve, we just append directly.
-      // the reason is it seems like Parcel 2 just goes through
-      // and replaces content hash with string replace and adds a `/`
-      // in front of it. So if we do url resolve then we get a double
-      // `//` and the link won't work.
-      const absoluteImageUrl = ogUrlContent + ogImageContent;
-
-      const ogImageTagAbsoluteUrl = ogImageTag.replace(
-        ogImageContent,
-        absoluteImageUrl,
-      );
-
-      const twitterImageTagAbsoluteUrl = twitterImageTag.replace(
-        twitterImageContent,
-        absoluteImageUrl,
-      );
-
+      let patchedHtml = html;
       // we assume the twitter:image meta tag has the same URL as the og:image tag
-      const patchedHtml = html
-        .replace(ogImageTag, ogImageTagAbsoluteUrl)
-        .replace(twitterImageTag, twitterImageTagAbsoluteUrl);
-
+      patchedHtml = findReplaceMeta(patchedHtml, 'og:image');
+      patchedHtml = findReplaceMeta(patchedHtml, 'twiiter:image');
       asset.setCode(patchedHtml);
     } catch (error) {
       throw new Error(error.message);
